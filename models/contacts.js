3b399c0237e5a8e4b65bb555b72c7506be4e36b8
roxananecsoi <radu.roxana07@gmail.com>
2024-10-07 14:46:36 +0300

    completare tema

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,80 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { v4: uuidv4 } = require("uuid");
 
-const listContacts = async () => {}
+const CONTACTS_FILE_PATH = "./models/contacts.json";
 
-const getContactById = async (contactId) => {}
+const saveContactsToFile = async (contactsData) => {
+  try {
+    await fs.writeFile(
+      CONTACTS_FILE_PATH,
+      JSON.stringify(contactsData, null, 2)
+    );
+    console.log("Contacts saved successfully!");
+  } catch (error) {
+    console.error("Error saving contacts:", error);
+    throw new Error("Failed to save contacts");
+  }
+};
 
-const removeContact = async (contactId) => {}
+const readContactsFromFile = async () => {
+  try {
+    const contactsJSON = await fs.readFile(CONTACTS_FILE_PATH);
+    return JSON.parse(contactsJSON);
+  } catch (error) {
+    console.error("Error reading contacts:", error);
+    throw new Error("Failed to read contacts");
+  }
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  return await readContactsFromFile();
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await readContactsFromFile();
+  return contacts.find((element) => element.id === contactId);
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await readContactsFromFile();
+  const updatedContacts = contacts.filter(
+    (element) => element.id !== contactId
+  );
+  await saveContactsToFile(updatedContacts);
+  console.log(`Contact with id: ${contactId} removed successfully!`);
+  return updatedContacts;
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await readContactsFromFile();
+    const preparedContact = {
+      id: uuidv4(),
+      ...body,
+    };
+    contacts.push(preparedContact);
+    await saveContactsToFile(contacts);
+    console.log("Contact added successfully!");
+  } catch (error) {
+    throw new Error(`Error adding contact: ${error.message}`);
+  }
+};
+
+const updateContact = async (updatedContact, contactId) => {
+  try {
+    const contacts = await readContactsFromFile();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      throw new Error("The contact was not found.");
+    }
+    const updatedFields = { ...contacts[index], ...updatedContact };
+    contacts[index] = { ...updatedFields, id: contactId };
+    await saveContactsToFile(contacts);
+    return contacts[index];
+  } catch (error) {
+    throw new Error(`Error updating contact: ${error.message}`);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +82,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
